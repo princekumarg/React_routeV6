8ace07aa0144bb03491508a05ee9be8c266d8242
prince kumar <agarwal8789@gmail.com>
2024-02-08 21:26:35 +0530

    commit made

--- ReactChapter2/src/App.jsx
+++ ReactChapter2/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Post from "./components/pages/Post";
+import Layout from "./components/Layout";
+import PostLayout from "./components/PostLayout";
+import PostDetail from "./components/pages/PostDetail";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="post" element={<PostLayout />}>
+            <Route path=":category" element={<PostDetail />}/>
+            <Route index element={<Post />}/>
+          </Route>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<h1>Error 404 Page not Found !!</h1>} />
+      </Routes>
+    </BrowserRouter>
+   </>
   )
 }
 
