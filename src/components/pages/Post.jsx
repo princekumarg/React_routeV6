c3404bd4c4d4aa1a7f5626ceb648b7901dc6fb52
prince kumar <agarwal8789@gmail.com>
2024-02-08 16:30:22 +0530

    commit made

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -1,9 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useParams ,useSearchParams} from "react-router-dom";
 const Post = () => {
-  let {category}=useParams();
+  let {category,id}=useParams();
+  let [searchParams,setSearchParams]=useSearchParams();
+  console.log(searchParams);
   return (
     <>
       <h1>Post Pages -{category}</h1>
+      <h1>Post Pages -{id}</h1>
     </>
   )
 }
